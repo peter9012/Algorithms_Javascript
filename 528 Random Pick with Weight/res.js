455d393b1dbe596d5e27392b44be802d8364fb62
peter9012 <rambo_lu@hotmail.com>
2022-07-28 13:09:24 +0800

    528 Random Pick with Weight

---
+++ 528 Random Pick with Weight/res.js
@@ -0,0 +1,26 @@
+var Solution = function(w) {
+  this.weights =[]
+  this.sum=0;
+  for (let i=0; i<w.length; i++) {
+    this.sum += w[i];
+    this.weights.push(this.sum)
+  }
+};
+
+Solution.prototype.pickIndex = function() {
+  let index = Math.floor(Math.random() * this.sum);
+    let arr = this.weights
+    let start = 0, end = arr.length - 1
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2)
+        if(index < arr[mid]){
+            end = mid - 1
+        }else{
+            start = mid + 1
+        }
+    }
+    return start
+};
+
+const res = new Solution([1,3]);
+console.log(res.pickIndex());
