31e42e37a4ae37c1741c3ae3235e93f2de1ad1a1
peter9012 <rambo_lu@hotmail.com>
2022-08-02 20:02:16 +0800

    977 Squares of a Sorted Array

---
+++ 977 Squares of a Sorted Array/res.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function(nums) {
+  let n = nums.length;
+  let result = [];
+  let left = 0;
+  let right = n - 1;
+  
+  for (let i = n - 1; i >= 0; i--) {
+    let square;
+    if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+      square = nums[right];
+      right--;
+    } else {
+      square = nums[left];
+      left++;
+    }
+    result[i] = square * square;
+  }
+  return result;
+};
