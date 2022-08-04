2ae8b5ba043b88cbd49824d6b5389f2e05fb38b3
peter9012 <rambo_lu@hotmail.com>
2022-08-04 14:23:17 +0800

    242 Valid Anagram

---
+++ 242 Valid Anagram/res.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if(s.length !== t.length) return false;
+  const resSet = new Array(26).fill(0);
+  const base = "a".charCodeAt();
+  for(const i of s) {
+    resSet[i.charCodeAt() - base]++;
+  }
+  for(const i of t) {
+    if(!resSet[i.charCodeAt() - base]) return false;
+    resSet[i.charCodeAt() - base]--;
+  }
+  return true;
+};
+
+const s = "anagram";
+const t = "nagaram";
+console.log(isAnagram(s,t));
