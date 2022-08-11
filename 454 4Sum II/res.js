05562923f50a9c20688d260db82255057cd56ee3
peter9012 <rambo_lu@hotmail.com>
2022-08-11 12:45:36 +0800

    454 4Sum II

---
+++ 454 4Sum II/res.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+ var fourSumCount = function(nums1, nums2, nums3, nums4) {
+  const twoSumMap = new Map();
+  let count = 0;
+
+  for(const n1 of nums1) {
+      for(const n2 of nums2) {
+          const sum = n1 + n2;
+          twoSumMap.set(sum, (twoSumMap.get(sum) || 0) + 1)
+      }
+  }
+
+  for(const n3 of nums3) {
+      for(const n4 of nums4) {
+          const sum = n3 + n4;
+          count += (twoSumMap.get(0 - sum) || 0)
+      }
+  }
+
+  return count;
+};
+
+const nums1 = [1, 2];
+const nums2 = [-2, -1];
+const nums3 = [-1, 2];
+const nums4 = [0, 2];
+console.log(fourSumCount(nums1, nums2, nums3, nums4));
