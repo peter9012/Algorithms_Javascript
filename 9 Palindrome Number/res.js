cf072ed45c4011fa15e137837b083c8f1a99b728
peter9012 <rambo_lu@hotmail.com>
2022-09-07 00:54:35 -0700

    Palindrome Number

---
+++ 9 Palindrome Number/res.js
@@ -0,0 +1,10 @@
+var isPalindrome = function(x) {
+  if (x < 0) return false
+
+  let rev = 0
+  for(let i = x; i >= 1; i = Math.floor(i/10)) rev = rev*10 + i%10
+  return rev === x
+};
+
+const input = 121
+console.log(isPalindrome(input));
